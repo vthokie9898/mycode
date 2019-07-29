694c39fc2d2b4d87fe7fa122d994c1a8f39529cd
vthokie9898 <bengals7@gmail.com>
2019-07-29 13:50:26 -0400

    changes

---
+++ lab_input/iptaker02.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+user_input=input("please enter an IPv4 IP address")
+
+## the line below creates a single string that is passed to print()
+# print("You told me the IPv4 address is: " + user_input)
+
+##print() can be given a series of objects seperated by a comma
+print("You told me the IPv4 address is: ", user_input)
+
+vendor_name=input("Who dis?")
+print("Whos dis is: " + vendor_name)
+
