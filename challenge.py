23a3aa40f9c5152f1626286bdab33e2c5f3ecd22
vthokie9898 <bengals7@gmail.com>
2019-07-30 08:35:14 -0400

    challenge update

--- challenge.py
+++ challenge.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python3
 ships=['Santa-Maria', 'Pinta', 'Nina', 3]
-print(f"Columbus had {ships[3]} ships: the {ships[2]}, the {ships[1]}, and the {ships[0]}")
+print(f"Columbus had {ships[3]} ships: the {ships[2]}, the {ships[1]}, and the {ships[0]}.")
 
