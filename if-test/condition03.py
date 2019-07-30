a897a6bad0ff12a1cf1caae73b874f5376c2e3f4
vthokie9898 <bengals7@gmail.com>
2019-07-30 13:44:05 -0400

    If statement

---
+++ if-test/condition03.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+hostname = input("What value should we set for hostname? ")
+
+## Notice how the next line has changed
+## here we use the str.lower() method to return a lowercase string
+if hostname.lower() == "mtg":
+    print("The hostname was found to be mtg")
+    print("The hostname matches the expected config.")
+
+print("Exiting script")
+
+
+
