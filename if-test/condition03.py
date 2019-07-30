89477702d60c70b48d4cfeecdb9715c36de6b59f
vthokie9898 <bengals7@gmail.com>
2019-07-30 14:00:25 -0400

    If statement

--- if-test/condition03.py
+++ if-test/condition03.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
+
+# Author:  Mike Trotta
+# If statement
+
 hostname = input("What value should we set for hostname? ")
 
 ## Notice how the next line has changed
@@ -6,7 +10,10 @@ hostname = input("What value should we set for hostname? ")
 if hostname.lower() == "mtg":
     print("The hostname was found to be mtg")
     print("The hostname matches the expected config.")
+else:
+    print("You are so dumb!  For real!")
 
+# Always print out to the user at the end of a script
 print("Exiting script")
 
 
