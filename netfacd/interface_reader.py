f003727ccee84c3dd8ef5c9121c4f3c8c2efb53e
vthokie9898 <bengals7@gmail.com>
2019-08-01 13:16:41 -0400

    interfaces and MAC

--- netfacd/interface_reader.py
+++ netfacd/interface_reader.py
@@ -10,3 +10,26 @@ for i in netifaces.interfaces():
          print(netifaces.ifaddresses(i) [netifaces.AF_INET] [0] ['addr']) # prints the IP address
      except:
          print('Could not collect adapter information') # Print an error message
+
+print("\n" * 5)
+x = input("What adapter do you want the IP address of? ")
+def IP(interface):
+    try:
+        print('IP: ', end="") # This print statement will always print IP without an end of line
+        print(netifaces.ifaddresses(interface) [netifaces.AF_INET] [0] ['addr']) # prints the IP address
+    except:
+        print('Could not collect adapter information') # Print an error message
+
+IP(x)
+
+print("\n" * 5)
+y = input("What adapter do you want the MAC address of? ")
+def MAC(address):
+    try:
+        print('MAC: ', end="") # This print statement will always print IP without an end of line
+        print(netifaces.ifaddresses(address) [netifaces.AF_LINK] [0] ['addr']) # prints the IP address
+    except:
+        print('Could not collect adapter information') # Print an error message
+
+MAC(x)
+
