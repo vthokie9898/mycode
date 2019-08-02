2e33a4e017e53c69b5350ca6ba62473b467c40c4
vthokie9898 <bengals7@gmail.com>
2019-08-02 08:38:56 -0400

    another update

--- iss/ride_iss.py
+++ iss/ride_iss.py
@@ -1,29 +1,22 @@
 #!/usr/bin/env python3
-
+"""lab from Chad"""
 import urllib.request
 import json
-
 ## define url
 MAJORTOM = 'http://api.open-notify.org/astros.json'
-
 def main():
-
+    """main function"""
     ## Call the webservice
     groundctrl = urllib.request.urlopen(MAJORTOM)
-
     ## put fileobject into helmet
     helmet = groundctrl.read()
-
     ## decode json to python data source
     helmetson = json.loads(helmet.decode('utf-8'))
-
     ## display our pythonic data
     print("\n\nConverted python data")
     print(helmetson)
-
     print("\n\nPeople in space: ", helmetson['number'])
     people = helmetson['people']
-    for i in people:
-        print(i['name'] + ' on the ' + i['craft'])
-    
+    for x in people:
+        print(x['name'] + ' is on the ' + x['craft'])
 main()
