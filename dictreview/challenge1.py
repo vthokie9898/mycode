c449db4ff7470c065d23a5fe74596e3dd01675fa
vthokie9898 <bengals7@gmail.com>
2019-09-16 15:41:23 -0400

    update

--- dictreview/challenge1.py
+++ dictreview/challenge1.py
@@ -8,4 +8,15 @@ def main():
             'colors': ['red', 'white', 'blue']}
 
     print(f"I have {cars.get('number')} cars, a {cars['colors'][0]} {cars['vehicles'][0]['brand']} {cars['vehicles'][0]['model']}, a {cars['colors'][1]} {cars['vehicles'][1]['brand']} {cars['vehicles'][1]['model']}, and a {cars['colors'][2]} {cars['vehicles'][2]['brand']} {cars['vehicles'][2]['model']}.")
+
+    print('***************************************************************')
+
+    c = cars['colors']
+    v = cars['vehicles']
+    m = 'model'
+    b = 'brand'
+
+    print(f"I have {cars['number']} cars, a {c[0]} {v[0][b]} {v[0][m]}, a {c[0]} {v[1][b]} {v[1][m]}, and a {c[2]} {v[2][b]} {v[2][m]}.")
+
+
 main()
