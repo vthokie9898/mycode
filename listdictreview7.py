3248b511f9f644693a61e24dd8cc41971d840666
vthokie9898 <bengals7@gmail.com>
2019-09-17 09:50:30 -0400

    listdict

--- listdictreview7.py
+++ listdictreview7.py
@@ -1,1 +1,21 @@
+#!/usr/bin/env python3
 
+def main():
+
+    my_var = []
+    your_var = {}
+
+    my_var += ['fluffy', 'kaya', 'max']
+    my_var.append('butch')
+    my_var.extend('tom')
+    print(my_var)
+    print(my_var[0::2])
+
+    your_var.update({'dog': 'cujo'})
+    your_var['lizards'] = ['larry', 'spot', 'beardy']
+    print(your_var)
+    print(your_var['lizards'][1])
+
+    del your_var['dog']
+    print(your_var)
+main()
