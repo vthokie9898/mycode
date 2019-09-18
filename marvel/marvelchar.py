10e9c2a66acee359ae949ab29df3a0a947367d63
vthokie9898 <bengals7@gmail.com>
2019-09-18 09:05:54 -0400

    marvelchars

--- marvel/marvelchar.py
+++ marvel/marvelchar.py
@@ -17,4 +17,38 @@ def hashbuilder(tim, pvkee, pubkee):
 ## http://gateway.marvel.com/v1/public/characters
 ## ?name=Spider-Man&ts=1&apikey=1234&hash=ffd275c
 def marvelcharcall(stampystamp, hashyhash, pkeyz, lookup):
-    r = requests.get(CHAR+"?name="+lookup+"&ts="stampystamp+"&apikey="pkeyz+"&hash="+hashyhash)
+    r = requests.get(CHAR+"?name="+lookup+"&ts="+stampystamp+"&apikey="+pkeyz+"&hash="+hashyhash)
+    print(CHAR+"?name="+lookup+"&ts"+stampystamp+"&apikey"+pkeyz+"&hash"+hashyhash)
+    return r.json()
+
+def main():
+
+    #harvest private key
+    with open(args.dev) as mccoy:
+        beast = mccoy.read().rstrip('\n')
+
+    ## harvest public key
+    with open(args.pub) as munroe:
+        storm = munroe.read().rstrip('\n')
+
+    ## create an integer from a float timestamp (for our RAND)
+    nightcrawler = str(time.time()).rstrip('.')
+
+    ## build hash with hashbuilder(timestamp, privatekey, publickey)
+    cerebro = hashbuilder(nightcrawler, beast, storm)
+
+    ## call the API with marvelcharcall(timestamp, hash, publickey, character)
+    uncannyxmen = marvelcharcall(nightcrawler, cerebro, storm, "Wolverine")
+
+    ## display results
+    print(uncannyxmen)
+
+## Define arguments to collect
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dev', \
+  help='Provide the /path/to/file.priv containing Marvel private developer key')
+    parser.add_argument('--pub', \
+  help='Provide the /path/to/file.pub containing Marvel public developer key')
+    args = parser.parse_args()
+    main()
