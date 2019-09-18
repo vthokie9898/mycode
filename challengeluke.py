0cba338137db3ba990b81e1acc0738a86ab86117
vthokie9898 <bengals7@gmail.com>
2019-09-18 13:16:46 -0400

    starwars challenge

--- challengeluke.py
+++ challengeluke.py
@@ -1,12 +1,19 @@
 import requests
+import argparse
+
 
 NAME = 'https://swapi.co/api/people/10/?format=json'
 
 def main():
-    r = requests.get(NAME)
-    starwars = {}
-    print(r.json())
-    starwars.update(r.json())
-    print(f'{starwars["name"]} is {starwars["hair_color"]}')
+    r = requests.get(f'https://swapi.co/api/people/1/?format=json')
+    data = r.json()
+    print(f'{data["name"]} is {data["hair_color"]}')
+
+
+    parser = argparse.ArgumentParser(description="Add a gender attribute")
+    parser.add_argument('-att', metavar='attribute', type=str, help="Ask for any attribute")
+    args = parser.parse_args()
+    force_json = requests.get(NAME).json()
+    print(f'Name - {force_json.get["name"]}\n Hair Color - {force_json.get["hair_color"]}\n Optional Attribute:{args.att} - {force_json.get(args.att)}')
 
 main()
--- challengestrwars.py
+++ challengestrwars.py
@@ -1,7 +1,10 @@
 import requests
+import argparse
 
 for i in range(1, 10):
     r = requests.get(f'https://swapi.co/api/people/{i}/?format=json')
     data = r.json()
     #print(data)
     print(f"{data['name']} is {data['hair_color']}")
+
+
